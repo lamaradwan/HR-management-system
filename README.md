46a2324dd33394423a29eedac1b3ac54250f7932
Lama Radwan <lama.radwan96@gmail.com>
2022-04-24 18:45:37 +0300

    added a grid and a form to add a new employees

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # HR-management-system
 
 ## Day 07 - Part 1 of the task:
-1. Create the HR Management system home page and accounting page
-2. Generating a constructor and a prototype function to calculate the employees salaries
-3. print the employees names with their salaries on the home page
-3. leave the accounting page empty for the next part of the task
+1. Created the HR Management system home page and accounting page
+2. Added a constructor and a prototype function to calculate the employees salaries
+3. printed the employees names with their salaries on the home page
+3. left the accounting page empty for the next part of the task
+
+## Day 08 - Part 2 of the task:
+1. Created a grid cards for the existed and the newly added employees
+2. Added a form for filling the user informations
+3. Done some styling and added some icons from font awesome
 
 - What observations or questions do you have about what you’ve learned so far?
 
@@ -12,4 +17,4 @@ The teaching team is doing a great work
 
 - How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
 
-I thought it will take two hours and it took me 3 hours
+I thought it will take 3 hours and it took me 4 hours
--- app.js
+++ app.js
@@ -7,18 +7,16 @@ function Employee(ID,fullName, department,level,imageUrl){
     this.level = level;
     this.imageUrl = imageUrl;
 }
-
 //Adding the Employees
-let ghazi = new Employee(1000,'Ghazi Samer','Administration','Senior','//imgURL');
-let lana = new Employee(1001,'Lana Ali','Finance','Senior','//imgURL');
-let tamara = new Employee(1002,'Tamara Ayoub','Marketing','Senior','//imgURL');
-let safi = new Employee(1003,'Safi Walid','Marketing', 'Mid-Senior','//imgURL');
-let omar = new Employee(1004,'Omar Zaid','Development','Senior','//imgURL');
-let rana = new Employee(1005,'Rana Saleh','Development','Junior','//imgURL');
-let hadi = new Employee(1006,'Hadi Ahmad','Finance','Mid-Senior','//imgURL');
-
+let ghazi = new Employee(1000,'Ghazi Samer','Administration','Senior','Assets/Ghazi.jpeg');
+let lana = new Employee(1001,'Lana Ali','Finance','Senior','Assets/Lana.jpeg');
+let tamara = new Employee(1002,'Tamara Ayoub','Marketing','Senior','Assets/Tamara.jpeg');
+let safi = new Employee(1003,'Safi Walid','Marketing', 'Mid-Senior','Assets/Safi.jpeg');
+let omar = new Employee(1004,'Omar Zaid','Development','Senior','Assets/Omar.jpeg');
+let rana = new Employee(1005,'Rana Saleh','Development','Junior','Assets/Rana.jpeg');
+let hadi = new Employee(1006,'Hadi Ahmad','Finance','Mid-Senior','Assets/Hadi.jpeg');
 //Defining a function to calculate the net salary after deducting the tax value
-Employee.prototype.salary = function(level){
+Employee.prototype.salary = function(){
     let netSalary;
     let tax = 7.5/100;
     let value;
@@ -37,15 +35,56 @@ Employee.prototype.salary = function(level){
         value = netSalary*tax;
         netSalary -= value;
     }
-    return netSalary;
+    return Math.floor(netSalary);
 }
-
+//Defining a function to generate a random number for IDs
+let randomID = function randIDs(){
+    let x = 0;
+    x= Math.floor(Math.random()*(2000-1007)+1007);
+    return x;
+}
+//Defining a function for getting the values in the fields then add it in the DOM
+let addEmployee = function(event){
+    event.preventDefault();
+    let emp;
+    console.log('submit clicked');
+    let fnameInput = document.getElementById('fullName').value;
+    let deptInput = document.getElementById('department').value;
+    let levelInput = document.getElementById('level').value;
+    let imageInput = document.getElementById('imageURL').value;
+    emp = new Employee(randomID(),fnameInput,deptInput,levelInput,imageInput);
+    emp.print();
+}
 //Defining a function to print the employees
 Employee.prototype.print= function(){
-    document.write(`<h2>${this.fullName}  - ${this.salary()} <br>`);
+    //Creating and styling the div
+    let div = document.createElement('div');
+    div.style.width = '250px';
+    div.style.height = '290px';
+    div.style.backgroundColor = 'rgb(145, 196, 255)';
+    div.style.borderRadius = '10px'
+    div.style.boxShadow = '0.5px 0.5px 5px gray'
+    div.style.margin = '20px'
+    div.style.overscrollBehaviorInline = 'backgroundColor=white;'
+    //Creating and styling the images 
+    let profileImage = document.createElement('img');
+    profileImage.setAttribute('src',this.imageUrl);
+    profileImage.style.width= '180px';
+    profileImage.style.height= '180px';
+    profileImage.style.marginLeft = '36px';
+    profileImage.style.marginTop = '20px';
+    //Creating and styling the employee information
+    let info = document.createElement('p');
+    info.innerHTML = `${this.fullName} - ID:${this.ID} - Department: ${this.department} - Level:${this.level} - Salary: ${this.salary()}`;
+    info.style.fontSize = '14px';
+    info.style.marginLeft = '30px';
+    info.style.marginTop = '5px';
+    //Inserting the element into the HTML page
+    let main = document.getElementById('grid');
+    main.appendChild(div);
+    div.appendChild(profileImage);
+    div.appendChild(info);
 }
-
-
 //Calling the printing function for the selected employees
 ghazi.print();
 lana.print();
@@ -54,3 +93,6 @@ safi.print();
 omar.print();
 rana.print();
 hadi.print();
+//Calling the handler submit button function
+let submitBtn = document.getElementById('submitBtn');
+submitBtn.addEventListener('click', addEmployee);
